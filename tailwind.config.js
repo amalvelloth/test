8cba8672a81cdd05e03d5742281a07a69b5ab104
amal velloth <amalv414@gmail.com>
2025-02-02 19:59:12 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {
-        fontfamily:{
-          title: ["DMsans", "sans-serif"],
-        }
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '500px', // Custom 'xs' breakpoint
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
